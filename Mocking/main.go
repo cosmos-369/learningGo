4365887dcc61722b0689866fe0a0b99c79019e47
cosmos-369 <cosmos333999@gmail.com>
2023-11-03 15:20:14 +0530

    completed Mocking

---
+++ Mocking/main.go
@@ -0,0 +1,63 @@
+package main
+
+import (
+	"fmt"
+	"io"
+	"os"
+	"time"
+)
+
+const (
+	countdownStart = 3
+	finalWord      = "Go!"
+
+	sleep = "sleep"
+	write = "write"
+)
+
+type Sleeper interface {
+	Sleep()
+}
+
+type SpyCountdownOperations struct {
+	Calls []string
+}
+
+func (s *SpyCountdownOperations) Sleep() {
+	s.Calls = append(s.Calls, sleep)
+}
+
+func (s *SpyCountdownOperations) Write(p []byte) (n int, err error) {
+	s.Calls = append(s.Calls, write)
+	return
+}
+
+type ConfigurableSleeper struct {
+	duration time.Duration
+	sleep    func(time.Duration)
+}
+
+func (c *ConfigurableSleeper) Sleep() {
+	c.sleep(c.duration)
+}
+
+type SpyTime struct {
+	durationSlept time.Duration
+}
+
+func (s *SpyTime) Sleep(d time.Duration) {
+	s.durationSlept = d
+}
+
+func Countdown(writer io.Writer, sleeper Sleeper) {
+	for i := countdownStart; i > 0; i-- {
+		fmt.Fprintln(writer, i)
+		sleeper.Sleep()
+	}
+	fmt.Fprintf(writer, finalWord)
+}
+
+func main() {
+	sleeper := ConfigurableSleeper{1 * time.Second, time.Sleep}
+	Countdown(os.Stdout, &sleeper)
+}
